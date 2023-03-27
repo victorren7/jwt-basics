11b3665d31563b2167e3831ee3e199dccf48c072
victor <31532226+victorren7@users.noreply.github.com>
2023-03-27 15:10:34 -0700

    added a few pre-req files

---
+++ middleware/error-handler.js
@@ -0,0 +1,10 @@
+const CustomAPIError = require('../errors/custom-error')
+
+const errorHandlerMiddleWare = async (err, req, res, next) => {
+  if (error instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({msg: err.message})
+  }
+    return res.status(500).json({ msg: 'Something went wrong, please try again.'})
+}
+
+module.exports = errorHandlerMiddleWare
