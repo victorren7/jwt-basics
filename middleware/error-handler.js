5d85b7a99d6ae424230e28f6e84dbbc6d02ee250
victor <31532226+victorren7@users.noreply.github.com>
2023-03-30 12:42:15 -0700

    added login and dashboard api's

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,10 @@
 const CustomAPIError = require('../errors/custom-error')
 
 const errorHandlerMiddleWare = async (err, req, res, next) => {
-  if (error instanceof CustomAPIError) {
+  if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({msg: err.message})
   }
+  console.log('res', res)
     return res.status(500).json({ msg: 'Something went wrong, please try again.'})
 }
 
